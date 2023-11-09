80527f6c2de1a579f72a39f7956a99536d30602f
Giovanna Vieira <126898074+giivieira@users.noreply.github.com>
2023-11-09 09:35:36 -0300

    Add files via upload

--- api/v1/endpoints/curso.py
+++ api/v1/endpoints/curso.py
@@ -24,3 +24,56 @@ async def post_curso(curso: CursoSchema, db: AsyncSession = Depends(get_session)
     db.add(novo_curso)
     await db.commit()
     return novo_curso
+
+#DELETE CURSO
+@router.delete('/{curso_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_del = result.scalar_one_or_none()
+        if curso_del:
+            await session.delete(curso_del)
+            await session.commit()
+            return Response(status_code=status.HTTP_204_NO_CONTENT)
+        else:
+            raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado..."))
+        
+#GET CURSOS 
+@router.get('/', response_model=List[CursoSchema])
+async def get_cursos(db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel)
+        result = await session.execute(query)
+        cursos: List[CursoModel] = result.scalars().all()
+        return cursos
+    
+#GET CURSO
+@router.get('/{curso_id}', response_model=CursoSchema,
+            status_code=status.HTTP_200_OK)
+async def get_curso(curso_id: int, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso = result.scalar_one_or_none()
+        if curso:
+            return curso
+        else:
+            raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado..."))
+        
+#PUT CURSO
+@router.put('/{curso_id}', response_model=CursoSchema, status_code=status.HTTP_202_ACCEPTED)
+async def put_curso(curso_id: int, curso: CursoSchema, db: AsyncSession = Depends(get_session)):
+    async with db as session:
+        query = select(CursoModel).filter(CursoModel.id == curso_id)
+        result = await session.execute(query)
+        curso_up = result.scalar_one_or_none()
+        if curso_up:
+            curso_up.titulo = curso.titulo
+            curso_up.aulas = curso.aulas
+            curso_up.horas = curso.horas
+            curso_up.instrutor = curso.instrutor
+            await session.commit()
+            return curso_up
+        else:
+            raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Curso não encontrado..."))
